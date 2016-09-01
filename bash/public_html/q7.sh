3378a0de769eedfb30c410f8c46ce612345a0c61
komal-webonise <komal.lunkad@weboniselab.com>
2016-09-01 14:03:12 +0530

    html and bootstrap assignment

--- bash/public_html/q7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -R public_html public_html
-
-cd public_html
-
-mv public_html renamed_dir
--- bash/public_html/renamed_dir/q6.sh
+++
@@ -1,4 +0,0 @@
-for file in $(find . -type f | head -4)
-do 
-	mv $file public_html
-done
